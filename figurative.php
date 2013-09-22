a212eb5afb0b56e30c00a18382471aec276ad7aa
nadine <neauro@gmail.com>
2013-09-22 13:40:55 -0700

    spacing

--- figurative.php
+++ figurative.php
@@ -10,15 +10,12 @@
           $desc = fopen($descfile, "r");
           $thumbnail = $filename . "thumbnail.jpg"; 
           $desc_text = fgets($desc, 4096);
-        ?>
-
+        ?> 
           <a class="fancybox buttons" title="<?= $desc_text ?>" data-fancybox-group="button" href="<?= $thumbnail ?>">
             <div style="background-image:url(<?= $thumbnail ?>);"></div>
           </a>
         <?php
-          }
-      ?>
-
+          } ?> 
     </div>
   </div>
 </div>

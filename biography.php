c019537b8b3f29b8c7bf8c88b513098d5043ccc1
nadine <neauro@gmail.com>
2013-09-22 16:29:29 -0700

    spelling errors

--- biography.php
+++ biography.php
@@ -1,13 +1,13 @@
 <?php include "include/_header.php"; ?> 
 
 <div id="container">
-  <h1>Biography</h1>
+  <h2>Biography</h2>
 <p>
-  Fong move to Seattle from Taiwan in 1987. She has an associate degree in business, but later pursued her passion in fine arts and graduated from the University of Washington Painting program.  She has had shows and received awards during her art career, including Best in Show from the GAGE Art Academy.  In addition to her painting, she also taught after school art programs for middle schoolers, and also helps run community program workshops.  Her works are mainly portraiture and figurative.  She finds the artistic rendition of people to be a satisfyingly complex challenge, and also a celebration of the emotional deliverance of the human spirit.
+  Fong moved to Seattle from Taiwan in 1987. She has an Associate's Degree in Business, but later pursued her passion in fine arts and graduated from the University of Washington Fine Arts in Painting program. She has had shows and received awards during her art career, including Best in Show from the GAGE Art Academy. In addition to her painting, she has also taught afterschool art programs for middle schoolers, and helps run community program workshops. Her works are mainly portraiture and figurative. She finds the artistic rendition of people to be a satisfyingly complex challenge, and also a celebration of the emotional deliverance of the human spirit.
 </p>
 
 <p>
-  Fong lives in Seattle with her three children and her husband.  She currently gives private lessons at her own studio, and frequently enjoys participating in open studios around the city. 
+  Fong lives in Seattle with her three children and her husband. She currently gives private lessons at her own studio, and frequently enjoys participating in open studios around the city. 
 </p>
 </p>
 </div>

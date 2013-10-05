3d9f998889244c65a310d16d1821c38ef1e12042
nadine <neauro@gmail.com>
2013-10-05 15:57:14 -0700

    different background position

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <div id="main">
   <a href="figurative.php"><div class="landing" style="background-image:url('content/figurative/5/thumbnail.jpg');"><p>figurative</p></div></a>
   <a href="portrait.php"><div class="landing" style="background-image:url('content/portrait/12/thumbnail.jpg');background-position:71%;"><p>portrait</p></div></a>
-  <a href="stilllife.php"><div class="landing" style="background-image:url('content/still_life/4/thumbnail.jpg');background-position:29%;"><p>still life</p></div></a>
+  <a href="stilllife.php"><div class="landing" style="background-image:url('content/still_life/28/thumbnail.jpg');background-position:70%;"><p>still life</p></div></a>
   <a href="photography.php"><div class="landing" style="background-image:url('content/photography/14/thumbnail.jpg');background-position:29%;"><p>photography</p></div></a>
   <a href="sketches.php"><div class="landing" style="background-image:url('content/charcoal/3/thumbnail.jpg');background-position:29%;"><p>sketches</p></div></a>
   <a href="chineseink.php"><div class="landing" style="background-image:url('content/ink/2/thumbnail.jpg');background-position:71%;"><p>chinese ink</p></div></a>

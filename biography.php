8cff96f74a9353b8e4305216e41be4b57a17afb9
nadine <neauro@gmail.com>
2014-10-05 13:53:18 -0700

    updated bio

--- biography.php
+++ biography.php
@@ -2,19 +2,16 @@
 
 <div id="container">
   <h2>Biography</h2>
-<p>
-  Fong moved from Taiwan to Seattle in 1987. She earned an associates degree in business, but later pursued her passion of fine arts and graduated from the University of Washington painting program in 2000. She has had several shows and received various awards during her art career, including Best in Show from the GAGE Art Academy. In addition to her personal painting, she has taught after school art programs and helps run community program workshops.  Her works are mainly portraiture and figurative.  She finds the artistic rendition of people to be interesting and a satisfyingly complex challenge.
-</p>
+<p>Fong moved from Taiwan to Seattle in 1987, and graduated from the University of Washington Painting Program in 2000. She has been featured in several shows and has received various awards during her art career. In addition to her time spent painting, she has taught after school art programs and helped run community program workshops. Her pieces are mainly portraiture and figurative with an impressionistic emphasis. She finds the artistic rendition of people to be a satisfyingly complex challenge.  </p>
 
-<p>
-  Fong lives in Seattle with her three children and husband.  Beside exploring and experiencing her own painting, she currently gives lessons and frequently participates in open studios around the city.
-</p>
+<p>Aside from exploring and experiencing her own painting style, she currently teaches lessons and hosts open studios at her workspace in Magnuson Park.  </p>
 
   <h2 class="contact">Contact</h2>
 <p>
-Fong Baatz<br/>
+www.fongbaatz.com<br/>
+<a href="http://facebook.com/fong.baatz" target="_new">facebook.com/fong.baatz</a><br/>
 E-mail: <a href="mailto:fongbaatz@yahoo.com">fongbaatz@yahoo.com</a></br>
-Location: <a href="https://www.google.com/maps/preview#!q=Plant+Amnesty&data=!1m4!1m3!1d1284!2d-122.2617438!3d47.6825221!4m32!2m14!1m13!1s0x0%3A0xef84f511437cffa9!3m8!1m3!1d1284!2d-122.2615132!3d47.6827026!3m2!1i1273!2i734!4f13.1!4m2!3d47.682693!4d-122.2606337!5m16!2m15!1m14!1s0x5490130a2c4843e7%3A0x613bb9df75359633!2smagnuson+park+building+30!3m8!1m3!1d2567!2d-122.2608346!3d47.6812562!3m2!1i1273!2i734!4f13.1!4m2!3d47.681353!4d-122.259335">Magnuson Park, Building 30</a>
+Location: <a href="https://www.google.com/maps/preview#!q=Plant+Amnesty&data=!1m4!1m3!1d1284!2d-122.2617438!3d47.6825221!4m32!2m14!1m13!1s0x0%3A0xef84f511437cffa9!3m8!1m3!1d1284!2d-122.2615132!3d47.6827026!3m2!1i1273!2i734!4f13.1!4m2!3d47.682693!4d-122.2606337!5m16!2m15!1m14!1s0x5490130a2c4843e7%3A0x613bb9df75359633!2smagnuson+park+building+30!3m8!1m3!1d2567!2d-122.2608346!3d47.6812562!3m2!1i1273!2i734!4f13.1!4m2!3d47.681353!4d-122.259335" target="_new">Magnuson Park, Building 30</a>
 </p>
 
 </div>
